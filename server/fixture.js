01b2411ef2bb196623a48a27f8f428d8e6e84d81
Ahmad Abd Arrahman <ahmad.a.arrahman@gmail.com>
2014-06-11 14:39:39 +0300

    added fixture posts

---
+++ server/fixture.js
@@ -0,0 +1,415 @@
+if (Posts.find().count() === 0) {
+    Posts.insert({
+        "from": {
+            "_id": "10203679286603182",
+            "name": "Ahmad Abd Arrahman"
+        },
+        "message": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "created_time": "2014-05-30T12:59:41+0000",
+        "updated_time": "2014-05-30T12:59:41+0000",
+        "likes": {
+            "data": [{
+                "_id": "630790407011642",
+                "name": "Mohamed Hakem"
+            }, {
+                "_id": "10202326631275635",
+                "name": "Sarra Khairy"
+            }, {
+                "_id": "10203893352434643",
+                "name": "Mohamed Ayman"
+            }, {
+                "_id": "620248794720247",
+                "name": "Maha Elatfy"
+            }, {
+                "_id": "10204192489279267",
+                "name": "Mohamed Elnbrawy"
+            }, {
+                "_id": "10202832481507022",
+                "name": "Ahmed M. Abd El-azeem"
+            }, {
+                "_id": "723107357752155",
+                "name": "Mahmmoud Mahdi"
+            }, {
+                "_id": "10203275895650841",
+                "name": "Ahmed Adly Saad"
+            }, {
+                "_id": "10201240699738224",
+                "name": "محمد مصطفى ابوليله"
+            }, {
+                "_id": "10203559585410868",
+                "name": "Abdulrahman Hamdi"
+            }, {
+                "_id": "802012346498170",
+                "name": "Konan Ahmed"
+            }, {
+                "_id": "643646729018323",
+                "name": "Mohammed Selim"
+            }, {
+                "_id": "778298188880965",
+                "name": "Eng Ahmed ELshrkawy"
+            }, {
+                "_id": "662113630508965",
+                "name": "Eng-Ahmed Wahba"
+            }]
+        },
+        "comments": {
+            "data": [{
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "message": "علي فكرة هو لو مرسي فاشل وماكنش هيسيطر علي مؤسسات البلد كانوا سابوه يخربط لانهم كانوا هيعملوا اللي عاوزينه وهو ف التوهان\nلكن هما انقلبوا عليه لما لاقوه هينضف البلد\nلكن انا معاك بانه من الاسباب اللي ودتنا للي احنا فيه",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:34:14+0000",
+                "like_count": 1,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203743421766521",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "ما علينا يا محمد في ليه أنا بأقول عليه فاشل... بس هأقولك سبب واحد يكفي إني     \" السيسي\" و \"محمد إبراهيم\" أكبر مجرمين ف البلد هو اللي جايبهم و كان بيشكر  فيهم كأنهم من الأولياء. ده بالنسبه ليا إسمه فشل.",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:47:11+0000",
+                "like_count": 1,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203743455127355",
+                "from": {
+                    "_id": "802012346498170",
+                    "name": "Konan Ahmed"
+                },
+                "message": "اسمه عته يا أحمد مش فشل ... لكن يبقى انه الكلام من خارج المنظومة أسهل بكثير جدا من الكلام من داخلها",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:55:03+0000",
+                "like_count": 1,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203743474367836",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "مش عاوز أدخل ف الجدل عن مفهوم الفشل أو بشكل أعم رأيي في مرسي و إدارته... الخلاصة إنه هو حتى الأن _بالعقل و الحسابات البسيطة_ رئيس البلد دي",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:59:24+0000",
+                "like_count": 1,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203743878617942",
+                "from": {
+                    "_id": "802012346498170",
+                    "name": "Konan Ahmed"
+                },
+                "message": "بالضبط ... وأى عته او ادعاء من أصحاب نظرية ماشى انقلاب بس مرسى ميرجعش اسمه عودة لممارسة العته والتخبط الفاضى ... يعنى مفيش حاجة اسمها ان البلد دى هتتخلص من فكرة الانقلابات العسكرية للابد الا برجوع رئيسها الشرعى شاء من شاء وأبى من أبى ...لانه فى حينها فكرة أى انقلاب عسكرى ستمحى محوا ...",
+                "can_remove": true,
+                "created_time": "2014-05-30T15:13:14+0000",
+                "like_count": 0,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203744556794896",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "احمد عبدالرحمن مع الشرعيه",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:28+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744558954950",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "احمد عبدالرحمن ضد الانقلاب",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:40+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744559874973",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "الانقلاب اخد السلم معاه فوق",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:51+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744732479288",
+                "from": {
+                    "_id": "620248794720247",
+                    "name": "Maha Elatfy"
+                },
+                "message": "معقولا!!!",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:47:31+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203752566515134",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "انا عاوز أقولكم إن الإنقلاب بيترنح عشان عرف الكلام اللي انا بقوله ده",
+                "can_remove": true,
+                "created_time": "2014-05-31T20:24:08+0000",
+                "like_count": 2,
+                "user_likes": false
+            }]
+        },
+        "shares": {
+            "data": [{
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }]
+        }
+    })
+
+    Posts.insert({
+        "from": {
+            "_id": "10203679286603182",
+            "name": "Basem Saad"
+        },
+        "message": "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "created_time": "2014-05-30T12:59:41+0000",
+        "updated_time": "2014-05-30T12:59:41+0000",
+        "likes": {
+            "data": [{
+                "_id": "630790407011642",
+                "name": "Mohamed Hakem"
+            }, {
+                "_id": "10202326631275635",
+                "name": "Sarra Khairy"
+            }, {
+                "_id": "10203893352434643",
+                "name": "Mohamed Ayman"
+            }, {
+                "_id": "620248794720247",
+                "name": "Maha Elatfy"
+            }, {
+                "_id": "10204192489279267",
+                "name": "Mohamed Elnbrawy"
+            }, {
+                "_id": "10202832481507022",
+                "name": "Ahmed M. Abd El-azeem"
+            }, {
+                "_id": "723107357752155",
+                "name": "Mahmmoud Mahdi"
+            }, {
+                "_id": "10203275895650841",
+                "name": "Ahmed Adly Saad"
+            }, {
+                "_id": "10201240699738224",
+                "name": "محمد مصطفى ابوليله"
+            }, {
+                "_id": "10203559585410868",
+                "name": "Abdulrahman Hamdi"
+            }, {
+                "_id": "802012346498170",
+                "name": "Konan Ahmed"
+            }, {
+                "_id": "643646729018323",
+                "name": "Mohammed Selim"
+            }, {
+                "_id": "778298188880965",
+                "name": "Eng Ahmed ELshrkawy"
+            }, {
+                "_id": "662113630508965",
+                "name": "Eng-Ahmed Wahba"
+            }]
+        },
+        "comments": {
+            "data": [{
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "message": "علي فكرة هو لو مرسي فاشل وماكنش هيسيطر علي مؤسسات البلد كانوا سابوه يخربط لانهم كانوا هيعملوا اللي عاوزينه وهو ف التوهان\nلكن هما انقلبوا عليه لما لاقوه هينضف البلد\nلكن انا معاك بانه من الاسباب اللي ودتنا للي احنا فيه",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:34:14+0000",
+                "like_count": 1,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203743421766521",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "ما علينا يا محمد في ليه أنا بأقول عليه فاشل... بس هأقولك سبب واحد يكفي إني     \" السيسي\" و \"محمد إبراهيم\" أكبر مجرمين ف البلد هو اللي جايبهم و كان بيشكر  فيهم كأنهم من الأولياء. ده بالنسبه ليا إسمه فشل.",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:47:11+0000",
+                "like_count": 1,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203743455127355",
+                "from": {
+                    "_id": "802012346498170",
+                    "name": "Konan Ahmed"
+                },
+                "message": "اسمه عته يا أحمد مش فشل ... لكن يبقى انه الكلام من خارج المنظومة أسهل بكثير جدا من الكلام من داخلها",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:55:03+0000",
+                "like_count": 1,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203743474367836",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "مش عاوز أدخل ف الجدل عن مفهوم الفشل أو بشكل أعم رأيي في مرسي و إدارته... الخلاصة إنه هو حتى الأن _بالعقل و الحسابات البسيطة_ رئيس البلد دي",
+                "can_remove": true,
+                "created_time": "2014-05-30T13:59:24+0000",
+                "like_count": 1,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203743878617942",
+                "from": {
+                    "_id": "802012346498170",
+                    "name": "Konan Ahmed"
+                },
+                "message": "بالضبط ... وأى عته او ادعاء من أصحاب نظرية ماشى انقلاب بس مرسى ميرجعش اسمه عودة لممارسة العته والتخبط الفاضى ... يعنى مفيش حاجة اسمها ان البلد دى هتتخلص من فكرة الانقلابات العسكرية للابد الا برجوع رئيسها الشرعى شاء من شاء وأبى من أبى ...لانه فى حينها فكرة أى انقلاب عسكرى ستمحى محوا ...",
+                "can_remove": true,
+                "created_time": "2014-05-30T15:13:14+0000",
+                "like_count": 0,
+                "user_likes": false
+            }, {
+                "_id": "10203743191040753_10203744556794896",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "احمد عبدالرحمن مع الشرعيه",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:28+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744558954950",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "احمد عبدالرحمن ضد الانقلاب",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:40+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744559874973",
+                "from": {
+                    "_id": "10204192489279267",
+                    "name": "Mohamed Elnbrawy"
+                },
+                "message": "الانقلاب اخد السلم معاه فوق",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:17:51+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203744732479288",
+                "from": {
+                    "_id": "620248794720247",
+                    "name": "Maha Elatfy"
+                },
+                "message": "معقولا!!!",
+                "can_remove": true,
+                "created_time": "2014-05-30T17:47:31+0000",
+                "like_count": 2,
+                "user_likes": true
+            }, {
+                "_id": "10203743191040753_10203752566515134",
+                "from": {
+                    "_id": "10203679286603182",
+                    "name": "Ahmad Abd Arrahman"
+                },
+                "message": "انا عاوز أقولكم إن الإنقلاب بيترنح عشان عرف الكلام اللي انا بقوله ده",
+                "can_remove": true,
+                "created_time": "2014-05-31T20:24:08+0000",
+                "like_count": 2,
+                "user_likes": false
+            }]
+        },
+        "shares": {
+            "data": [{
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }, {
+                "_id": "10203743191040753_10203743368125180",
+                "from": {
+                    "_id": "643646729018323",
+                    "name": "Mohammed Selim"
+                },
+                "created_time": "2014-05-30T13:34:14+0000",
+                "type": "fb",
+                "destination_link": "https://www.facebook.com/thenextweb/posts/10152502693163523"
+            }]
+        }
+    })
+};
